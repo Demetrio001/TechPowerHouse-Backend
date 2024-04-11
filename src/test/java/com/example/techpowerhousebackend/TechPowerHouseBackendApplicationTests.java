c2bb2b580b9158e0487bbf6a7412377340501a57
romeo <rmodtr01l13h224y@studenti.unical.it>
2024-04-11 16:23:57 +0200

    Initial commit

---
+++ src/test/java/com/example/techpowerhousebackend/TechPowerHouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techpowerhousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechPowerHouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
